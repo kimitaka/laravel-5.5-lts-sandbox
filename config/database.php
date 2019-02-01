2445ad8917fc7222deed36bb1b1c75d9ca797141
Kimitaka Watanabe <167225+kimitaka@users.noreply.github.com>
2019-02-02 01:53:22 +0900

    Change UNIX socket which is listen by MySQL

--- config/database.php
+++ config/database.php
@@ -46,13 +46,12 @@ return [
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
+            'unix_socket' => env('DB_SOCKET', '/run/mysqld/mysqld.sock'),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'strict' => true,
             'engine' => null,
-            'unix_socket' => '/run/mysqld/mysqld.sock',
         ],
 
         'pgsql' => [
